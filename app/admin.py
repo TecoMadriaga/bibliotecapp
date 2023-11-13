6cffbf590671badcc0f484c3c94fc75dda0769bf
Sergio Madriaga Ureta <smadriagau@outlook.com>
2023-11-13 20:07:59 -0300

    Update gitignore and admin, templates, and views files

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Libro, Autor, Editorial, Categoria
 
-# Register your models here.
+# Clase de personalización para el modelo Libro
+class LibroAdmin(admin.ModelAdmin):
+    # Campos que se muestran en la lista de objetos
+    list_display = ('titulo', 'editorial', 'anio_publicacion')
+
+    # Campos por los que se puede filtrar en la lista
+    list_filter = ('editorial', 'anio_publicacion', 'categorias')
+
+    # Campos por los que se puede buscar en la lista
+    search_fields = ('titulo', 'autores__nombre')
+
+    # Organizar los campos en la vista de detalle
+    fieldsets = (
+        (None, {
+            'fields': ('titulo', 'autores', 'editorial')
+        }),
+        ('Información Adicional', {
+            'fields': ('anio_publicacion', 'categorias'),
+            'classes': ('collapse',),
+        }),
+    )
+
+# Registra el modelo Libro con la personalización
+admin.site.register(Libro, LibroAdmin)
+
+# Registra otros modelos de manera normal
+admin.site.register(Autor)
+admin.site.register(Editorial)
+admin.site.register(Categoria)
